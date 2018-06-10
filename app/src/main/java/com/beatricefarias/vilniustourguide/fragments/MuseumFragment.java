c9e76aabe2ae79e24153b84fd8eecc72b9f0b004
beatricefar <beatricefarias.lt@gmail.com>
2018-06-11 00:25:53 +0300

    Fixed string resources conversion.

--- app/src/main/java/com/beatricefarias/vilniustourguide/fragments/MuseumFragment.java
+++ app/src/main/java/com/beatricefarias/vilniustourguide/fragments/MuseumFragment.java
@@ -36,34 +36,36 @@ public class MuseumFragment extends Fragment {
 
         museums.add(new Attraction("KGB Museum", R.drawable.vilnius_lt_logo,
                 "Aukų g. 2A",
-                "" + R.string.kgb_museum_desc,
+                getString(R.string.kgb_museum_desc),
                 Constants.ATTRACTION_IS_PAID));
         museums.add(new Attraction("Money museum", R.drawable.vilnius_lt_logo,
                 "Totorių g. 2 / 8",
-                "" + R.string.money_museum_desc,
+                getString(R.string.money_museum_desc),
                 Constants.ATTRACTION_IS_FREE));
         museums.add(new Attraction("National Museum of Lithuania", R.drawable.vilnius_lt_logo,
                 "Arsenalo g. 1",
-                "" + R.string.national_museum_desc,
+                getString(R.string.national_museum_desc),
                 Constants.ATTRACTION_IS_PAID));
         museums.add(new Attraction("Museum of Illusions", R.drawable.vilnius_lt_logo,
                 "Vokiečių g. 6",
-                "" + R.string.illusions_museum_desc,
+                getString(R.string.illusions_museum_desc),
                 Constants.ATTRACTION_IS_PAID));
         museums.add(new Attraction("National Gallery of Art", R.drawable.vilnius_lt_logo,
                 "Konstitucijos pr. 22",
-                "" + R.string.national_art_gallery_desc,
+                getString(R.string.national_art_gallery_desc),
                 Constants.ATTRACTION_IS_PAID));
         museums.add(new Attraction("Museum of Applied Arts and Design", R.drawable.vilnius_lt_logo,
                 "Arsenalo g. 3A",
-                "" + R.string.applied_arts_museum_desc,
+                getString(R.string.applied_arts_museum_desc),
                 Constants.ATTRACTION_IS_PAID));
         museums.add(new Attraction("Palace of The Grand Dukes of Lithuania", R.drawable.vilnius_lt_logo,
                 "Katedros a. 4",
-                "" + R.string.grand_dukes_palace_museum_desc, Constants.ATTRACTION_IS_PAID));
+                getString(R.string.grand_dukes_palace_museum_desc),
+                Constants.ATTRACTION_IS_PAID));
         museums.add(new Attraction("Telia Nonmuseum", R.drawable.vilnius_lt_logo,
                 "Vokiečių g. 4",
-                "" + R.string.telia_museum_desc, Constants.ATTRACTION_IS_PAID));
+                getString(R.string.telia_museum_desc),
+                Constants.ATTRACTION_IS_PAID));
 
         AttractionAdapter adapter = new AttractionAdapter(getActivity(), museums);
         ListView attractionList= rootView.findViewById(R.id.attraction_list);
